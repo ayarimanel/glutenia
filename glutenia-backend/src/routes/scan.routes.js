8b5ac6b2af500bbb156435706bd42cd7329b9f4b
ayarimanel <manelayari04@gmail.com>
2026-06-29 02:34:39 +0100

    Fix scan route: import verifyToken from correct middleware path

--- glutenia-backend/src/routes/scan.routes.js
+++ glutenia-backend/src/routes/scan.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { scanLabel } = require("../controllers/scan.controller");
-const { verifyToken } = require("../middleware/auth");
+const { verifyToken } = require("../middleware/verifyToken");
 
 router.post("/label", verifyToken, scanLabel);
 
